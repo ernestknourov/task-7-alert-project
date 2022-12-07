f91cb9e9a4f8205c52075b706d1ad861766a3c3b
ernestknourov <100434509+ernestknourov@users.noreply.github.com>
2022-12-07 08:24:40 +0100

    Update functions.py

--- functions.py
+++ functions.py
@@ -3,6 +3,9 @@ import config
 
 
 def read_logs(path: str) -> pd.DataFrame:
+    """
+    This function read logs_file from path and return dataframe with extracted data.
+    """
     data = pd.read_csv(path)
     data.columns = config.columns
     # data['date'] = pd.to_datetime(data['date'], unit='s')
@@ -11,9 +14,15 @@ def read_logs(path: str) -> pd.DataFrame:
 
 
 def rule1(df: pd.DataFrame, time: float) -> int:
+    """
+    This function return the number of errors within last minute.
+    """
     return len(df.loc[(time - 60 <= df['date']) & (df['date'] <= time) & (df['severity'] == "Error")])
 
 
 def rule2(df: pd.DataFrame, time: float):
+    """
+    This function return the number of errors on bundle_id within last hour
+    """
     return max(df.loc[(time - 3600 <= df['date']) & (df['date'] <= time)
                       & (df['severity'] == "Error")].groupby(['bundle_id'])['bundle_id'].count())
